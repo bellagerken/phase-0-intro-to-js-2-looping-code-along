4e94604a6a0b548f8f0792d78b216e80f8ced1e0
Bella Gerken <bellagerkenn@gmail.com>
2023-04-04 14:03:58 -0700

    completelooping code along lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const writeCards = (arr,event) =>
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages
+   
+
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"))
+
+
+function countDown(number) {
+    while(number > 0){
+        console.log(number)
+        number = number - 1
+    }
+}
+countDown(10);
